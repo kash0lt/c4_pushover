c0dbb3a594544c4f357fcf483dcaf68e81dff281
Kelly Holt <kasholt@gmail.com>
2020-12-08 14:51:03 -0700

    adding class defs

--- c4_pushover.py
+++ c4_pushover.py
@@ -5,13 +5,41 @@ from push_sec import Auth
 # https://www.pushover.net
 
 
+class C4PushOver():
+    def __init__(self):
+        self.conn = http.client.HTTPSConnection("api.pushover.net:443")
+
+    def OffLightOn(self):
+        self.conn.request("POST", "/1/messages.json",
+                          urllib.parse.urlencode({
+                              "token": auth.apitoken,
+                              "user": auth.usertoken,
+                              "message": "office light on",
+                              "title": "jibo office light on"
+                          }), {"Content-type": "application/x-www-form-urlencoded"})
+        self.conn.getresponse()
+
+    def OffLightOff(self):
+        self.conn.request("POST", "/1/messages.json",
+                          urllib.parse.urlencode({
+                              "token": auth.apitoken,
+                              "user": auth.usertoken,
+                              "message": "office light off",
+                              "title": "jibo office light off"
+                          }), {"Content-type": "application/x-www-form-urlencoded"})
+        self.conn.getresponse()
+
+    def OffRoomOff(self):
+        self.conn.request("POST", "/1/messages.json",
+                          urllib.parse.urlencode({
+                              "token": auth.apitoken,
+                              "user": auth.usertoken,
+                              "message": "office room off",
+                              "title": "jibo office room off"
+                          }), {"Content-type": "application/x-www-form-urlencoded"})
+        self.conn.getresponse()
+
+
 auth = Auth()
-conn = http.client.HTTPSConnection("api.pushover.net:443")
-conn.request("POST", "/1/messages.json",
-             urllib.parse.urlencode({
-                 "token": auth.apitoken,
-                 "user": auth.usertoken,
-                 "message": "office light on",
-                 "title": "jibo office light on"
-             }), {"Content-type": "application/x-www-form-urlencoded"})
-conn.getresponse()
+c4pushover = C4PushOver()
+c4pushover.OffRoomOff()
