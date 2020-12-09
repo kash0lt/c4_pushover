43cb03ad5edbe99e3203b5ceb01b2228752cdb68
Kelly Holt <kasholt@gmail.com>
2020-12-09 10:03:32 -0700

    streamlined and removed local references

--- c4_pushover.py
+++ c4_pushover.py
@@ -1,19 +1,24 @@
 import http.client
 import urllib
 from push_sec import Auth
-# push_sec contains the usertoken and api token for this python script to
+# push_sec contains the usertoken and api token for this python script to access
 # https://www.pushover.net
+# The class is simple with members apitoken, which is the API token given from pushover.net when the app was registered
+# and usertoken, which is the registered user token from pushover.net. Instanciate an instance of this class Auth() to
+# access these members.
 
 
 class C4PushOver():
-    def __init__(self):
+    def __init__(self, authref):
         self.conn = http.client.HTTPSConnection("api.pushover.net:443")
+        self.apiToken = authref.apitoken
+        self.userToken = authref.usertoken
 
     def OffLightOn(self):
         self.conn.request("POST", "/1/messages.json",
                           urllib.parse.urlencode({
-                              "token": auth.apitoken,
-                              "user": auth.usertoken,
+                              "token": self.apiToken,
+                              "user": self.userToken,
                               "message": "office light on",
                               "title": "jibo office light on"
                           }), {"Content-type": "application/x-www-form-urlencoded"})
@@ -22,8 +27,8 @@ class C4PushOver():
     def OffLightOff(self):
         self.conn.request("POST", "/1/messages.json",
                           urllib.parse.urlencode({
-                              "token": auth.apitoken,
-                              "user": auth.usertoken,
+                              "token": self.apiToken,
+                              "user": self.userToken,
                               "message": "office light off",
                               "title": "jibo office light off"
                           }), {"Content-type": "application/x-www-form-urlencoded"})
@@ -32,14 +37,13 @@ class C4PushOver():
     def OffRoomOff(self):
         self.conn.request("POST", "/1/messages.json",
                           urllib.parse.urlencode({
-                              "token": auth.apitoken,
-                              "user": auth.usertoken,
+                              "token": self.apiToken,
+                              "user": self.userToken,
                               "message": "office room off",
                               "title": "jibo office room off"
                           }), {"Content-type": "application/x-www-form-urlencoded"})
         self.conn.getresponse()
 
 
-auth = Auth()
-c4pushover = C4PushOver()
+c4pushover = C4PushOver(Auth())
 c4pushover.OffRoomOff()
